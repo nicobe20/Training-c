6c7489b0b665da0dec5ff6129b04cab41daa90f7
Nicolas Betancur <nicobetancurochoa@gmail.com>
2024-01-31 15:45:18 -0500

    While and do While

---
+++ Course on c/whileDo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// short code on the while do loop
+
+int main(){
+    int i; 
+    do{ 
+        printf("%d this is an infinite loop \n",i);
+        i++;
+    }
+    while (i<=5);
+
+
+
+}
---
+++ Course on c/whilec.c
@@ -0,0 +1,7 @@
+#include <stdio.h> 
+//small code to see the while function on c
+
+int main(){
+    
+
+}
